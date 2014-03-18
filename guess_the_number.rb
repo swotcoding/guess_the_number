f1edd39fd5312f5f17781926765827251dd0a218
swotcoding <swotcoding@gmail.com>
2014-03-18 18:05:05 +0000

    checked guess and secret number and returned a message

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,3 +1,12 @@
 puts "Welcome to Guess the Number!"
 secret_number = rand(6)
-puts secret_number
+
+puts "Guess a number between 0 and 5:"
+guess = gets.chomp.to_i
+
+if secret_number == guess
+  puts "Congratulations! You are correct"
+else
+  puts "Wrong! #{secret_number} was the correct number."
+end
+
